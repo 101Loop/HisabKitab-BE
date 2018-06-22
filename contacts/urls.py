6df91f645690a687df6908b0468590ce5266e526
Himanshu Shankar <me@himanshus.com>
2018-06-22 17:26:01 +0530

    Updated `requirements` Fixed `urls.py` in `Contacts`

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.Contact.as_view(), name='contact'),
+    url(r'', views.ShowContacts.as_view(), name='contact'),
 
 ]
