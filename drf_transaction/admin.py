07a64eef81204181214fef98b56177a6084f54ef
Sumit Singh <sumit.singh4613@gmail.com>
2023-02-05 13:02:48 +0530

    100% coverage for transaction app

--- drf_transaction/admin.py
+++ drf_transaction/admin.py
@@ -2,11 +2,5 @@ from django.contrib import admin
 
 from .models import TransactionDetail, TransactionMode
 
-
-class MyModelAdmin(admin.ModelAdmin):
-    def get_readonly_fields(self, request, obj=None):
-        return None if obj else "created_by"
-
-
 admin.site.register(TransactionMode)
 admin.site.register(TransactionDetail)
